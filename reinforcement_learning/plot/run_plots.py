2ccf06a47afc770cee938fe1b48107f08729471f
Ryan McCann <ryan_mccann@student.uml.edu>
2025-02-28 10:40:02 -0500

    Bug fixes for run_plots.py
    We were not tracking the algorithm name properly.
    
    Also, add an excel module.

--- reinforcement_learning/plot/run_plots.py
+++ reinforcement_learning/plot/run_plots.py
@@ -1,3 +1,5 @@
+import os
+
 from reinforcement_learning.utils.sim_filters import find_times
 from reinforcement_learning.utils.sim_data import load_blocking_data, load_rewards
 from reinforcement_learning.plot.blocking import plot_blocking_probabilities
@@ -9,8 +11,7 @@ def main():
     Controls the plots module.
     """
     dates_dict = {
-        '0227': 'NSFNet',
-        '0228': 'PanEuro'
+        '0228': 'NSFNet',
     }
 
     filter_dict = {
@@ -22,16 +23,23 @@ def main():
         ],
         'and_filter_list': [
             # Example: [['config', 'must_have'], 'expected_value']
+            ['path_algorithm', 'epsilon_greedy_bandit']
         ]
     }
     sims_info_dict = find_times(dates_dict=dates_dict, filter_dict=filter_dict)
     simulation_times = {}
-    for time_entry in sims_info_dict['times_matrix']:
+    for idx, time_entry in enumerate(sims_info_dict['times_matrix']):
         time_str = time_entry[0]
-        simulation_times.setdefault("Example Algorithm", []).append([time_str])
+        algorithms_matrix = sims_info_dict.get('algorithms_matrix', [])
+        if idx < len(algorithms_matrix) and algorithms_matrix[idx]:
+            algo_name = algorithms_matrix[idx][0]
+        else:
+            algo_name = "Unknown Algorithm"
+        simulation_times.setdefault(algo_name, []).append([time_str])
 
-    base_dir = "./data/output/NSFNet/0227"
-    base_logs_dir = "../../logs"
+    # TODO: (drl_path_agents) Hard coded date
+    base_dir = os.path.join('..', '..', 'data', 'output', 'NSFNet', '0228')
+    base_logs_dir = os.path.join('..', '..', 'logs')
 
     final_result = load_blocking_data(simulation_times, base_dir)
     rewards_data = load_rewards(simulation_times, base_logs_dir, base_dir)
