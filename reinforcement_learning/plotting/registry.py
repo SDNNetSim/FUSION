7276026a7fe0f0cf6452f72718143611a5568883
bozo-bud <theodore_thomson@student.uml.edu>
2025-05-20 18:49:43 -0400

    Implemented Throughput plots.

--- reinforcement_learning/plotting/registry.py
+++ reinforcement_learning/plotting/registry.py
@@ -14,7 +14,7 @@ from reinforcement_learning.plotting.lengths import plot_lengths_minmax
 from reinforcement_learning.plotting.mod_usage import plot_modulation_usage
 from reinforcement_learning.plotting.bw_block import plot_bw_blocked
 from reinforcement_learning.plotting.blocking import plot_blocking_stats_table
-from reinforcement_learning.plotting.link_data import plot_link_usage
+from reinforcement_learning.plotting.link_data import plot_link_usage, plot_link_throughput
 PLOTS = {
     "blocking": {"plot": plot_blocking_probabilities, "process": "process_blocking"},
     "memory": {"plot": plot_memory_usage, "process": "process_memory_usage"},
@@ -41,6 +41,10 @@ PLOTS = {
     },
     "link_usage": {
         "plot": plot_link_usage,
-        "process": "process_link_usage",
+        "process": "process_link_data",
+    },
+    "link_throughput": {
+        "plot": plot_link_throughput,
+        "process": "process_link_data"
     }
 }
