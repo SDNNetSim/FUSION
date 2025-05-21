c988d2631b8f86d8218fabf5aebb5e2d8a123885
Ryan McCann <ryan_mccann@student.uml.edu>
2025-05-21 18:45:04 -0400

    Cache GNN information
    Hopefully results in a significant increase in simulation training times.

--- reinforcement_learning/utils/gym_envs.py
+++ reinforcement_learning/utils/gym_envs.py
@@ -6,7 +6,7 @@ from reinforcement_learning.utils.setup import setup_rl_sim
 from reinforcement_learning.utils.callbacks import EpisodicRewardCallback, LearnRateEntCallback
 
 
-def create_environment():
+def create_environment(config_path: str = None):
     """
     Creates the simulation environment and associated callback for RL.
 
@@ -17,6 +17,6 @@ def create_environment():
 
     callback_list = CallbackList([ep_call_obj, param_call_obj])
     # TODO: I don't believe the callback is even used in sim env
-    env = SimEnv(render_mode=None, custom_callback=ep_call_obj, sim_dict=setup_rl_sim())
+    env = SimEnv(render_mode=None, custom_callback=ep_call_obj, sim_dict=setup_rl_sim(config_path=config_path))
     env.sim_dict['callback'] = callback_list
     return env, env.sim_dict, callback_list
