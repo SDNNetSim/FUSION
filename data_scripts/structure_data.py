0401b15dfb407d27b3d1018a20a180ced83872b9
Ryan McCann <ryan_mccann@student.uml.edu>
2025-06-06 08:21:23 -0400

    Format Spain topology correctly
    Also add DT topology

--- data_scripts/structure_data.py
+++ data_scripts/structure_data.py
@@ -76,15 +76,17 @@ def create_network(net_name: str, base_fp: str = None, const_weight: bool = Fals
             core_nodes_list = assign_core_nodes(core_nodes_fp=core_nodes_fp)
     elif net_name == 'Spainbackbone30':
         network_fp = os.path.join(base_fp, 'SPNB3014.txt')
-        if not is_only_core_node:
-            core_nodes_fp = os.path.join(base_fp, 'SPNB3014_core_nodes.txt')
-            core_nodes_list = assign_core_nodes(core_nodes_fp=core_nodes_fp)
+        # if not is_only_core_node:
+        #     core_nodes_fp = os.path.join(base_fp, 'SPNB3014_core_nodes.txt')
+        #     core_nodes_list = assign_core_nodes(core_nodes_fp=core_nodes_fp)
     elif net_name == 'geant':
         network_fp = os.path.join(base_fp, 'geant.txt')
     elif net_name == 'toy_network':
         network_fp = os.path.join(base_fp, 'toy_network.txt')
     elif net_name == 'metro_net':
         network_fp = os.path.join(base_fp, 'metro_net.txt')
+    elif net_name == 'dt_network':
+        network_fp = os.path.join(base_fp, 'dt_network.txt')
     else:
         raise NotImplementedError(f"Unknown network name. Expected USNet, NSFNet, or Pan-European. Got: {net_name}")
 
